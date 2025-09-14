db7b21e4139db408ffb15f5deb29f40151b4040e
Biswajyoti Ray <biswajyoti.4321.ray@gmail.com>
2025-09-14 10:13:11 +0530

    python to python3

--- trainVNet3times.sh
+++ trainVNet3times.sh
@@ -17,13 +17,13 @@ echo $epoch
 folder="Task_"${task}"_fully/"
 echo $folder
 
-#python code/train.py --task ${task} --exp ${folder}"vnet"${exp}/fold1 --seed 0 -g ${cuda} --base_lr ${lr} -ep ${epoch}
-#python code/test.py --task ${task} --exp ${folder}"vnet"${exp}/fold1 -g ${cuda}
-python code/evaluate_Ntimes.py --task ${task} --exp ${folder}"vnet"${exp} --folds 1
-#python code/train.py --exp ${folder}${exp}${task}/fold2 --seed 666 -g ${cuda} --base_lr ${lr} -ep ${epoch}
-#python code/test.py --exp ${folder}${exp}${task}/fold2 -g ${cuda}
-#python code/evaluate_Ntimes.py --exp ${folder}${exp}${task} --folds 2
-#python code/train.py --exp ${folder}${exp}${task}/fold3 --seed 2023 -g ${cuda} --base_lr ${lr} -ep ${epoch}
-#python code/test.py --exp ${folder}${exp}${task}/fold3 -g ${cuda}
+#python3 code/train.py --task ${task} --exp ${folder}"vnet"${exp}/fold1 --seed 0 -g ${cuda} --base_lr ${lr} -ep ${epoch}
+#python3 code/test.py --task ${task} --exp ${folder}"vnet"${exp}/fold1 -g ${cuda}
+python3 code/evaluate_Ntimes.py --task ${task} --exp ${folder}"vnet"${exp} --folds 1
+#python3 code/train.py --exp ${folder}${exp}${task}/fold2 --seed 666 -g ${cuda} --base_lr ${lr} -ep ${epoch}
+#python3 code/test.py --exp ${folder}${exp}${task}/fold2 -g ${cuda}
+#python3 code/evaluate_Ntimes.py --exp ${folder}${exp}${task} --folds 2
+#python3 code/train.py --exp ${folder}${exp}${task}/fold3 --seed 2023 -g ${cuda} --base_lr ${lr} -ep ${epoch}
+#python3 code/test.py --exp ${folder}${exp}${task}/fold3 -g ${cuda}
 
-#python code/evaluate_Ntimes.py --exp ${folder}${exp}${task}
+#python3 code/evaluate_Ntimes.py --exp ${folder}${exp}${task}
